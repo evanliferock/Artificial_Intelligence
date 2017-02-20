5ab749e821a38af08229ae21dadb764ba695b119
evanliferock <evanliferock@gmail.com>
2017-02-20 15:43:25 -0800

    Made it to be more like his example and added Parser class for easier work

--- parser.py
+++ parser.py
@@ -15,15 +15,14 @@ def main():
     Author 2: Patrick Chadbourne
     Author 3: Katie Phillips
     """
-    the_input = raw_input().strip()
-    if sentence(the_input):
+    if sentence(Parser()):
         print 'yes'
     else:
         print 'no'
 
 
 # **************************************************************
-def sentence(s):
+def sentence(p):
     """ Checks if s is a valid Sentence. Check for:
     P
     T/F
@@ -32,15 +31,26 @@ def sentence(s):
     :param s: A string that represents the sentence to check
     :return: boolean
     """
-    if len(s) == 0:
-        return True
-    elif is_symbol(s):
-        return True
-    elif len(s) > 1 and s[0] == '~':
-        return is_symbol(s[1]) and sentence(s[1:])
-    elif len(s) > 2 and (s[1] == '&' or s[1] == '|'
-            or s[1] == '=' or s[1] == '>'):
-        return is_symbol(s[0]) and sentence(s[2:])
+    return False
+
+
+# **************************************************************
+def negation_sentence(p):
+    """
+        checks for it to be a valid
+    :param p: parser class instance
+    :return: boolean
+    """
+    return False
+
+
+# **************************************************************
+def connective_sentence(p):
+    """
+        checks fot it to be a valid connective sentence
+    :param p: parser class instance
+    :return: boolean
+    """
     return False
 
 
@@ -54,6 +64,48 @@ def is_symbol(p):
     :return: boolean
     """
     return len(p) == 1 and p.isalpha()
+
+
+# **************************************************************
+class Parser:
+    """
+        A class to handle matching tokens and holding the input
+        Initializer gets input
+        match matches a passed in token with the recorded input
+        get_next gets the next token in the input
+    """
+    index = 0
+
+    def __init__(self):
+        """
+            sets up the parser by getting the input
+             removes whitespace from the input
+        """
+        self.the_input = raw_input().strip().replace(' ', '')
+
+    def match(self, token):
+        """
+            Matches the token to the current part of the input. Increments
+            index if successful
+        :param token: a string of length 1 that is to be matched
+        :return: boolean if match successful
+        """
+        try:
+            if self.the_input[self.index] == token:
+                self.index += 1
+                return True
+        except IndexError:
+            print 'Error on checking \'' + token + \
+                  '\': the input has been parsed'
+            return False
+        return False
+
+    def get_next(self):
+        """
+            fetches the next token in the input
+        :return: string of length 1 that is to be matched
+        """
+        return self.the_input[self.index]
 
 
 if __name__ == '__main__':
